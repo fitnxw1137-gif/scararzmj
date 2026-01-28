1e43d5354d467f68c73e1c8d9a94c11fc70c4f93
fitnxw1137-gif <fitnxw1137@gmail.com>
2026-01-28 13:06:53 +0300

    Add files via upload

---
+++ script.js.js
@@ -0,0 +1,13 @@
+// ================== FIREBASE КОНФИГУРАЦИЯ ==================
+const firebaseConfig = {
+    apiKey: "AIzaSyDkkAY3P35I-U1c0BeFfVj5piDfdKITi3g",
+    authDomain: "scarmjarz.firebaseapp.com",
+    databaseURL: "https://scarmjarz-default-rtdb.firebaseio.com",
+    projectId: "scarmjarz",
+    storageBucket: "scarmjarz.firebasestorage.app",
+    messagingSenderId: "847897340242",
+    appId: "1:847897340242:web:e68823b81e04990dcbb28b",
+    measurementId: "G-6WGVF47LPL"
+};
+
+// ... весь JavaScript код ...
